3be7788e95553103f58cb3dff0cda904e24d6b8e
Raddock <d.shvarev@mail.ru>
2023-01-07 01:40:09 +0300

    закончил главную и наши услуги, присутпил к тех.условиям

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() { 
+
+    $('.slider').slick({   
+        arrows:false,    
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 2000,
+    })
+
+})
