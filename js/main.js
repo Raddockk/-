840b24bbf4e011d243352e8b48c71db53330d791
Raddock <d.shvarev@mail.ru>
2023-01-08 20:49:52 +0300

    Приступил к адаптиву сайта, сделал модальное окно

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
     $('.slider').slick({   
         arrows:false,    
         dots: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 4000,
         speed: 2000,
     })
