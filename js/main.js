b582d6f4f0c2adeeb5e1f4bddc4918f779b0e9c8
Raddock <d.shvarev@mail.ru>
2023-02-12 22:39:29 +0300

    Осталось добавить фото к слайдеру

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ $(function () {
   })
 
 })
+
+$(function () {
+
+  $('.about-us__slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 2000,
+  })
+
+})
